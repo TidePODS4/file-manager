b0da1995137d994ef45c2d8d25f64f26298cced0
Павел Баканов <bakanov2210@icloud.com>
2023-09-19 23:49:01 +0300

    rename FileMetadataRepository

--- src/main/java/ru/server/filemanager/repository/FileMetadataRepository.java
+++ src/main/java/ru/server/filemanager/repository/FileMetadataRepository.java
@@ -2,12 +2,12 @@ package ru.server.filemanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.server.filemanager.entity.FileMetadata;
+import ru.server.filemanager.model.FileMetadata;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface FileRepository extends JpaRepository<FileMetadata, UUID> {
+public interface FileMetadataRepository extends JpaRepository<FileMetadata, UUID> {
     List<FileMetadata> findAllByParentId(UUID id);
 }

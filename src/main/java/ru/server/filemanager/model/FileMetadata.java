b065107ea973adf0c5c71f77aff123d10728fb56
Павел Баканов <bakanov2210@icloud.com>
2023-10-23 22:32:34 +0300

    added foreign key to file

--- src/main/java/ru/server/filemanager/model/FileMetadata.java
+++ src/main/java/ru/server/filemanager/model/FileMetadata.java
@@ -1,9 +1,13 @@
 package ru.server.filemanager.model;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -13,15 +17,25 @@ public class FileMetadata {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id")
+    @Nullable
     private UUID id;
 
     @Column(name = "parent_id")
     private UUID parentId;
 
     @Column(name = "name")
-    @NotBlank
+    @NotEmpty
     private String name;
 
     @Column(name = "is_folder")
+    @NotNull
     private boolean isFolder;
+
+    @ManyToMany
+    @JoinTable(
+            name = "file_user",
+            joinColumns = @JoinColumn(name = "file_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users;
 }

f404a7652fba929b6d5615ec0ff6d2c4ce485d22
Павел Баканов <bakanov2210@icloud.com>
2023-10-24 01:48:39 +0300

    added response status

--- src/main/java/ru/server/filemanager/exception/FileNotUploadedException.java
+++ src/main/java/ru/server/filemanager/exception/FileNotUploadedException.java
@@ -1,5 +1,9 @@
 package ru.server.filemanager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class FileNotUploadedException extends RuntimeException{
     public FileNotUploadedException(String message) {
         super(message);

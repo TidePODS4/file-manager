e1bc789a33f32ce9552ea14cc80597ac0e8d9ec8
Павел Баканов <bakanov2210@icloud.com>
2023-10-24 01:52:55 +0300

    to remove

--- src/main/java/ru/server/filemanager/exception/RoleDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package ru.server.filemanager.exception;
-
-public class RoleDoesNotExistException extends RuntimeException{
-    public RoleDoesNotExistException(String message) {
-        super(message);
-    }
-}

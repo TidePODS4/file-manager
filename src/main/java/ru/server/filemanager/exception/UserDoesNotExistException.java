664ded0c003068a065ce23dcdf70d843bb8cb094
Павел Баканов <bakanov2210@icloud.com>
2023-10-24 01:47:07 +0300

    added new exception

--- src/main/java/ru/server/filemanager/exception/UserDoesNotExistException.java
+++ src/main/java/ru/server/filemanager/exception/UserDoesNotExistException.java
@@ -1,5 +1,9 @@
 package ru.server.filemanager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class UserDoesNotExistException extends RuntimeException{
     public UserDoesNotExistException(String message) {
         super(message);

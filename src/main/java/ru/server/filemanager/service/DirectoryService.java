48bddf513ffb264236f41e5edd2662322ceed55d
Павел Баканов <bakanov2210@icloud.com>
2023-10-24 01:50:22 +0300

    new logic

--- src/main/java/ru/server/filemanager/service/DirectoryService.java
+++ src/main/java/ru/server/filemanager/service/DirectoryService.java
@@ -1,12 +1,13 @@
 package ru.server.filemanager.service;
 
 import ru.server.filemanager.model.FileMetadata;
+import ru.server.filemanager.model.User;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface DirectoryService {
-    List<FileMetadata> getFilesMetaByDirectoryId(UUID id);
+    List<FileMetadata> getFilesMetaByDirectoryId(UUID directoryId);
     List<FileMetadata> getRootFiles();
-    FileMetadata create(FileMetadata fileMetadata);
+//    FileMetadata create(FileMetadata fileMetadata);
 }
--- src/main/java/ru/server/filemanager/service/imp/DirectoryServiceImp.java
+++ src/main/java/ru/server/filemanager/service/imp/DirectoryServiceImp.java
@@ -1,38 +1,49 @@
 package ru.server.filemanager.service.imp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.server.filemanager.model.FileMetadata;
+import ru.server.filemanager.model.User;
 import ru.server.filemanager.repository.FileMetadataRepository;
 import ru.server.filemanager.service.DirectoryService;
+import ru.server.filemanager.service.UserService;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class DirectoryServiceImp implements DirectoryService {
     private final FileMetadataRepository fileMetadataRepository;
-
-    @Autowired
-    public DirectoryServiceImp(FileMetadataRepository fileMetadataRepository) {
-        this.fileMetadataRepository = fileMetadataRepository;
-    }
+    private final UserService userService;
 
     @Override
-    public List<FileMetadata> getFilesMetaByDirectoryId(UUID id){
-        return fileMetadataRepository.findAllByParentId(id);
+    public List<FileMetadata> getFilesMetaByDirectoryId(UUID directoryId){
+        UUID ownerId = userService.getUserIdBySecurityContext(
+                SecurityContextHolder
+                        .getContext()
+        );
+
+        return fileMetadataRepository.findAllByOwnerIdAndParentId(ownerId, directoryId);
     }
 
     @Override
     public List<FileMetadata> getRootFiles(){
-        return fileMetadataRepository.findAllByParentId(null);
+        UUID ownerId = userService.getUserIdBySecurityContext(
+                SecurityContextHolder
+                        .getContext()
+        );
+
+        return fileMetadataRepository.findAllByOwnerIdAndParentId(ownerId, null);
     }
 
-    @Override
-    @Transactional
-    public FileMetadata create(FileMetadata fileMetadata) {
-        return fileMetadataRepository.save(fileMetadata);
-    }
+//    @Override
+//    @Transactional
+//    public FileMetadata create(FileMetadata fileMetadata) {
+//        return fileMetadataRepository.save(fileMetadata);
+//    }
 }

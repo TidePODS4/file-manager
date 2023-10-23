e137f284ff30e2d2b1d93613d1b74500ff79633f
Павел Баканов <bakanov2210@icloud.com>
2023-10-24 01:51:11 +0300

    removed boilerplate code

--- src/main/java/ru/server/filemanager/controller/DirectoryManagerController.java
+++ src/main/java/ru/server/filemanager/controller/DirectoryManagerController.java
@@ -1,16 +1,17 @@
 package ru.server.filemanager.controller;
 
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import ru.server.filemanager.model.FileMetadata;
+import ru.server.filemanager.model.User;
 import ru.server.filemanager.service.DirectoryService;
-import ru.server.filemanager.service.imp.DirectoryServiceImp;
+import ru.server.filemanager.service.UserService;
 import ru.server.filemanager.dto.ErrorResponse;
 import ru.server.filemanager.exception.FileNotUploadedException;
 import ru.server.filemanager.util.validator.FileMetadataValidator;
@@ -20,20 +21,15 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/folders")
+@RequiredArgsConstructor
 public class DirectoryManagerController {
     private final DirectoryService directoryService;
+    private final UserService userService;
     private final FileMetadataValidator fileMetadataValidator;
 
-    @Autowired
-    public DirectoryManagerController(DirectoryServiceImp directoryService,
-                                      FileMetadataValidator fileMetadataValidator) {
-        this.directoryService = directoryService;
-        this.fileMetadataValidator = fileMetadataValidator;
-    }
-
     @GetMapping()
-    @PreAuthorize("hasRole(T(ru.server.filemanager.util.Role).ADMIN.getRole())")
-    public List<FileMetadata> getRootFiles(){
+//    @PreAuthorize("hasRole(T(ru.server.filemanager.util.Role).ADMIN.getRole())")
+    public List<FileMetadata> getRootFilesByUser(){
         return directoryService.getRootFiles();
     }
 
@@ -42,30 +38,30 @@ public class DirectoryManagerController {
         return directoryService.getFilesMetaByDirectoryId(id);
     }
 
-    @PostMapping()
-    public ResponseEntity<FileMetadata> createNewFolder(@RequestBody @Valid FileMetadata fileMetadata,
-                                                        BindingResult bindingResult){
-        fileMetadataValidator.validate(fileMetadata, bindingResult);
-
-        if (bindingResult.hasErrors()){
-            StringBuilder errorMsg = new StringBuilder();
-
-            List<FieldError> errors = bindingResult.getFieldErrors();
-
-            for (var error : errors){
-                errorMsg.append(error.getField()).append(" - ")
-                        .append(error.getDefaultMessage()).append(";");
-            }
-
-            throw new FileNotUploadedException(errorMsg.toString());
-        }
-
-        fileMetadata.setFolder(true);
-        var folderMetadata = directoryService.create(fileMetadata);
-
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(folderMetadata);
-    }
+//    @PostMapping()
+//    public ResponseEntity<FileMetadata> createNewFolder(@RequestBody @Valid FileMetadata fileMetadata,
+//                                                        BindingResult bindingResult){
+//        fileMetadataValidator.validate(fileMetadata, bindingResult);
+//
+//        if (bindingResult.hasErrors()){
+//            StringBuilder errorMsg = new StringBuilder();
+//
+//            List<FieldError> errors = bindingResult.getFieldErrors();
+//
+//            for (var error : errors){
+//                errorMsg.append(error.getField()).append(" - ")
+//                        .append(error.getDefaultMessage()).append(";");
+//            }
+//
+//            throw new FileNotUploadedException(errorMsg.toString());
+//        }
+//
+//        fileMetadata.setFolder(true);
+//        var folderMetadata = directoryService.create(fileMetadata);
+//
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//                .body(folderMetadata);
+//    }
 
     @ExceptionHandler
     private ResponseEntity<ErrorResponse> handleException(FileNotUploadedException ex){

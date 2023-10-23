3f3a250dbfb076bf9a1f56d8d007c1fc976b0154
Павел Баканов <bakanov2210@icloud.com>
2023-10-23 22:39:18 +0300

    testing new SecurityConfig

---
+++ src/main/java/ru/server/filemanager/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package ru.server.filemanager.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.server.filemanager.util.converter.JwtAuthConverter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
+//    private final JWTAuthenticationFilter jwtAuthFilter;
+//    private final JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+//
+//    @Bean
+//    public static PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//            AuthenticationConfiguration configuration) throws Exception {
+//        return configuration.getAuthenticationManager();
+//    }
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated();
+
+        http
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+//        http.csrf().disable()
+//                .authorizeHttpRequests((authorize) ->
+//                        //authorize.anyRequest().authenticated()
+//                        authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+//                                //.requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
+//                                .requestMatchers("/api/auth/**").permitAll()
+//                                .requestMatchers("/swagger-ui/**").permitAll()
+//                                .requestMatchers("/v3/api-docs/**").permitAll()
+//                                .anyRequest().authenticated()
+//
+//                ).exceptionHandling( exception -> exception
+//                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+//                ).sessionManagement( session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                );
+//
+//        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+    }
+
+}

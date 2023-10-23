c5d52a2b98e26350acbf88ec66b9d78700df3d70
Павел Баканов <bakanov2210@icloud.com>
2023-10-24 01:50:01 +0300

    bug fix

--- src/main/java/ru/server/filemanager/controller/FileManagerController.java
+++ src/main/java/ru/server/filemanager/controller/FileManagerController.java
@@ -6,7 +6,10 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import ru.server.filemanager.exception.FileNotFoundException;
+import ru.server.filemanager.model.FileMetadata;
 import ru.server.filemanager.service.imp.DirectoryServiceImp;
 import ru.server.filemanager.service.FileService;
 import ru.server.filemanager.service.imp.FileServiceImp;
@@ -15,10 +18,11 @@ import ru.server.filemanager.util.helper.FileHelper;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/file")
+@RequestMapping("/files")
 public class FileManagerController {
     private final FileService fileService;
     private final FileHelper fileHelper;
@@ -45,15 +49,22 @@ public class FileManagerController {
 
     private ResponseEntity<Resource> getResourceResponseEntity(
             @PathVariable("id") UUID id, boolean isDownload) throws IOException {
+
+        Optional<FileMetadata> fileMetadataOpt = fileService.getFileMetadataById(id);
+        FileMetadata fileMetadata = fileMetadataOpt.orElseThrow(
+                () -> new FileNotFoundException("File " + id + " not found")
+        );
+
         File file = fileService.getFileById(id);
-        String mimeType = fileHelper.getFileMimeType(file);
+        String mimeType = fileHelper.getFileMimeType(fileMetadata.getName());
         String headerType = isDownload ? "attachment" : "inline";
+
 
         Resource resource = new InputStreamResource(new FileInputStream(file));
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION,
-                        headerType + "; filename=\"" + file.getName() + "\"")
+                        headerType + "; filename=\"" + fileMetadata.getName() + "\"")
                 .contentLength(file.length())
                 .contentType(MediaType.parseMediaType(mimeType))
                 .body(resource);

371447927f171ba445ac3f1aaefc1a78d55e1369
Павел Баканов <bakanov2210@icloud.com>
2023-10-25 01:51:54 +0300

    added endpoint to create folder

--- src/main/java/ru/server/filemanager/controller/DirectoryManagerController.java
+++ src/main/java/ru/server/filemanager/controller/DirectoryManagerController.java
@@ -8,12 +8,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import ru.server.filemanager.dto.FolderDto;
+import ru.server.filemanager.exception.DirectoryNotCreatedException;
+import ru.server.filemanager.exception.UserDoesNotExistException;
 import ru.server.filemanager.model.FileMetadata;
-import ru.server.filemanager.model.User;
 import ru.server.filemanager.service.DirectoryService;
 import ru.server.filemanager.service.UserService;
 import ru.server.filemanager.dto.ErrorResponse;
 import ru.server.filemanager.exception.FileNotUploadedException;
+import ru.server.filemanager.util.ErrorBuilder;
 import ru.server.filemanager.util.validator.FileMetadataValidator;
 
 import java.util.List;
@@ -26,51 +29,58 @@ public class DirectoryManagerController {
     private final DirectoryService directoryService;
     private final UserService userService;
     private final FileMetadataValidator fileMetadataValidator;
+    private final ErrorBuilder errorBuilder;
 
     @GetMapping()
 //    @PreAuthorize("hasRole(T(ru.server.filemanager.util.Role).ADMIN.getRole())")
-    public List<FileMetadata> getRootFilesByUser(){
-        return directoryService.getRootFiles();
+    public ResponseEntity<List<FileMetadata>> getRootFiles(){
+        return ResponseEntity.ok(directoryService.getRootFiles());
     }
 
     @GetMapping("/{id}")
-    public List<FileMetadata> getFilesInDirectory(@PathVariable("id") UUID id){
-        return directoryService.getFilesMetaByDirectoryId(id);
+    public ResponseEntity<List<FileMetadata>> getFilesInDirectory(@PathVariable("id") UUID id){
+        return ResponseEntity.ok(directoryService.getFilesMetaByDirectoryId(id));
     }
 
-//    @PostMapping()
-//    public ResponseEntity<FileMetadata> createNewFolder(@RequestBody @Valid FileMetadata fileMetadata,
-//                                                        BindingResult bindingResult){
-//        fileMetadataValidator.validate(fileMetadata, bindingResult);
+    @PostMapping()
+    public ResponseEntity<FileMetadata> createNewFolder(@RequestBody @Valid FolderDto folderDto,
+                                                        BindingResult bindingResult){
+        var fileMetadata = directoryService.convertToEntity(folderDto);
+        var userId = userService.getUserIdBySecurityContext(SecurityContextHolder.getContext());
+        var userOpt = userService.getUserById(userId);
+        fileMetadata.setOwner(userOpt.orElseThrow(
+                () -> new UserDoesNotExistException("User "+ userId + " not found")));
+
+        fileMetadataValidator.validate(fileMetadata, bindingResult);
+
+        if (bindingResult.hasErrors()){
+            throw new DirectoryNotCreatedException(errorBuilder.buildErrorMsg(bindingResult));
+        }
+
+        var folderMetadata = directoryService.create(fileMetadata);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(folderMetadata);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFolder(@PathVariable("id") UUID id){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return HttpStatus.BAD_REQUEST.getReasonPhrase();
+    }
+
+//    @ExceptionHandler
+//    private ResponseEntity<ErrorResponse> handleException(FileNotUploadedException ex){
+//        ErrorResponse response = new ErrorResponse(
+//                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+//                ex.getMessage(),
+//                System.currentTimeMillis()
+//        );
 //
-//        if (bindingResult.hasErrors()){
-//            StringBuilder errorMsg = new StringBuilder();
-//
-//            List<FieldError> errors = bindingResult.getFieldErrors();
-//
-//            for (var error : errors){
-//                errorMsg.append(error.getField()).append(" - ")
-//                        .append(error.getDefaultMessage()).append(";");
-//            }
-//
-//            throw new FileNotUploadedException(errorMsg.toString());
-//        }
-//
-//        fileMetadata.setFolder(true);
-//        var folderMetadata = directoryService.create(fileMetadata);
-//
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(folderMetadata);
+//        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 //    }
-
-    @ExceptionHandler
-    private ResponseEntity<ErrorResponse> handleException(FileNotUploadedException ex){
-        ErrorResponse response = new ErrorResponse(
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                ex.getMessage(),
-                System.currentTimeMillis()
-        );
-
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
 }

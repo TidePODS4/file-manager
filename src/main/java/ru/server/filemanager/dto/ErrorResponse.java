963eb85653c2f741375e9e85b46d28cc14878d45
Павел Баканов <bakanov2210@icloud.com>
2023-10-25 01:47:11 +0300

    made class to record

--- src/main/java/ru/server/filemanager/dto/ErrorResponse.java
+++ src/main/java/ru/server/filemanager/dto/ErrorResponse.java
@@ -1,12 +1,4 @@
 package ru.server.filemanager.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class ErrorResponse {
-    private String code;
-    private String message;
-    private long timestamp;
+public record ErrorResponse(String code, String message) {
 }

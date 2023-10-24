952b89123a39632a6e267b970129e0b8f780b09a
Павел Баканов <bakanov2210@icloud.com>
2023-10-25 01:39:46 +0300

    added getUserById method

--- src/main/java/ru/server/filemanager/service/UserService.java
+++ src/main/java/ru/server/filemanager/service/UserService.java
@@ -1,10 +1,12 @@
 package ru.server.filemanager.service;
 
 import org.springframework.security.core.context.SecurityContext;
+import ru.server.filemanager.model.User;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
     UUID getUserIdBySecurityContext(SecurityContext securityContext);
+    Optional<User> getUserById(UUID id);
 }
--- src/main/java/ru/server/filemanager/service/imp/UserServiceImp.java
+++ src/main/java/ru/server/filemanager/service/imp/UserServiceImp.java
@@ -1,23 +1,34 @@
 package ru.server.filemanager.service.imp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import ru.server.filemanager.model.User;
+import ru.server.filemanager.repository.UserRepository;
 import ru.server.filemanager.service.UserService;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImp implements UserService {
+    private final UserRepository userRepository;
 
     @Override
     public UUID getUserIdBySecurityContext(SecurityContext securityContext) {
-        OidcUser principal = (OidcUser) securityContext
+        Jwt principal = (Jwt) securityContext
                 .getAuthentication()
                 .getPrincipal();
 
         return UUID.fromString(principal.getSubject());
+    }
+
+    @Override
+    public Optional<User> getUserById(UUID id) {
+        return userRepository.findById(id);
     }
 }

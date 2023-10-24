4016ad0c2abaeda0901ef18a26cb9a8ece3ae757
Павел Баканов <bakanov2210@icloud.com>
2023-10-25 01:40:19 +0300

    added response status to custom exceptions

--- src/main/java/ru/server/filemanager/exception/UserDoesNotExistException.java
+++ src/main/java/ru/server/filemanager/exception/UserDoesNotExistException.java
@@ -3,7 +3,7 @@ package ru.server.filemanager.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserDoesNotExistException extends RuntimeException{
     public UserDoesNotExistException(String message) {
         super(message);

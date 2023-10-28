49a718b8c5af965d68858f596e0810be2889f605
Павел Баканов <bakanov2210@icloud.com>
2023-10-28 15:53:51 +0300

    implemented save and delete files in file system

--- src/main/java/ru/server/filemanager/service/StorageService.java
+++ src/main/java/ru/server/filemanager/service/StorageService.java
@@ -1,5 +1,13 @@
 package ru.server.filemanager.service;
 
+import org.springframework.web.multipart.MultipartFile;
+import ru.server.filemanager.model.FileMetadata;
+
+import java.util.UUID;
+
 public interface StorageService {
     void createDirectory(String fullPath);
+    void saveFile(MultipartFile file, FileMetadata fileMetadata, String fullPath);
+    void deleteFile(String fullPath);
+    void deleteFolder(String fullPath);
 }
--- src/main/java/ru/server/filemanager/service/imp/StorageServiceImp.java
+++ src/main/java/ru/server/filemanager/service/imp/StorageServiceImp.java
@@ -1,17 +1,23 @@
 package ru.server.filemanager.service.imp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.server.filemanager.exception.DirectoryAlreadyExistsException;
-import ru.server.filemanager.exception.DirectoryNotCreatedException;
+import org.springframework.web.multipart.MultipartFile;
+import ru.server.filemanager.exception.*;
+import ru.server.filemanager.model.FileMetadata;
+import ru.server.filemanager.service.FileMetadataService;
 import ru.server.filemanager.service.StorageService;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class StorageServiceImp implements StorageService {
     @Override
     public void createDirectory(String fullPath) {
@@ -21,10 +27,56 @@ public class StorageServiceImp implements StorageService {
                     "Directory " + fullPath + " already exists");
 
         try {
-            Files.createDirectory(path);
+            Files.createDirectories(path);
         } catch (IOException e) {
             throw new DirectoryNotCreatedException(
                     "Directory " + fullPath + " not created");
         }
+    }
+
+    @Override
+    public void saveFile(MultipartFile file, FileMetadata fileMetadata, String fullPath) {
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(fullPath);
+            Files.write(path, bytes);
+        } catch (Exception e) {
+            throw new FileNotUploadedException("File " + file.getOriginalFilename() + " not uploaded");
+        }
+    }
+
+    @Override
+    public void deleteFile(String fullPath) {
+        Path path = Paths.get(fullPath);
+        if (!Files.exists(path))
+            throw new FileNotDeletedException("File not deleted");
+        File file = new File(fullPath);
+
+        boolean isDeleted = file.delete();
+
+        if (!isDeleted) {
+            throw new FileNotDeletedException("File not deleted");
+        }
+    }
+
+    @Override
+    public void deleteFolder(String fullPath) {
+        File directory = new File(fullPath);
+        if (!directory.exists())
+            throw new DirectoryNotFoundException("Folder " + fullPath + " not found");
+
+        File[] entries = directory.listFiles();
+
+        if (entries != null) {
+            for (File entry : entries) {
+                if (entry.isDirectory()) {
+                    deleteFolder(entry.getAbsolutePath());
+                } else {
+                    entry.delete();
+                }
+            }
+        }
+
+        directory.delete();
     }
 }

d5a988754a79db484165fcab649ee1685aee6e23
Павел Баканов <bakanov2210@icloud.com>
2023-10-28 15:56:41 +0300

    added converters and implemented delete folder

--- src/main/java/ru/server/filemanager/service/DirectoryService.java
+++ src/main/java/ru/server/filemanager/service/DirectoryService.java
@@ -1,15 +1,24 @@
 package ru.server.filemanager.service;
 
-import ru.server.filemanager.dto.FolderDto;
+import org.springframework.hateoas.CollectionModel;
+import ru.server.filemanager.dto.request.FolderDtoRequest;
+import ru.server.filemanager.dto.response.FileDtoResponse;
+import ru.server.filemanager.dto.response.FolderDtoResponse;
 import ru.server.filemanager.model.FileMetadata;
-import ru.server.filemanager.model.User;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DirectoryService {
     List<FileMetadata> getFilesMetaByDirectoryId(UUID directoryId);
     List<FileMetadata> getRootFiles();
+    Optional<FileMetadata> getDirectoryById(UUID id);
     FileMetadata create(FileMetadata fileMetadata);
-    FileMetadata convertToEntity(FolderDto folderDto);
+    FileMetadata convertToEntity(FolderDtoRequest folderDto);
+    FileDtoResponse convertToFileDtoResponse(FileMetadata fileMetadata) throws IOException;
+    FolderDtoResponse convertToFolderDtoResponse(FileMetadata fileMetadata) throws IOException;
+    CollectionModel<FileDtoResponse> convertToFileCollectionModel(List<FileDtoResponse> fileDtoResponses) throws IOException;
+    void deleteFolder(UUID id);
 }
--- src/main/java/ru/server/filemanager/service/imp/DirectoryServiceImp.java
+++ src/main/java/ru/server/filemanager/service/imp/DirectoryServiceImp.java
@@ -2,17 +2,25 @@ package ru.server.filemanager.service.imp;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.server.filemanager.dto.FolderDto;
+import ru.server.filemanager.dto.request.FolderDtoRequest;
+import ru.server.filemanager.dto.response.FileDtoResponse;
+import ru.server.filemanager.dto.response.FolderDtoResponse;
+import ru.server.filemanager.exception.DirectoryNotDeletedException;
+import ru.server.filemanager.exception.DirectoryNotFoundException;
+import ru.server.filemanager.exception.UserDoesNotExistException;
 import ru.server.filemanager.model.FileMetadata;
-import ru.server.filemanager.model.User;
 import ru.server.filemanager.repository.FileMetadataRepository;
 import ru.server.filemanager.service.*;
+import ru.server.filemanager.util.helper.HateoasLinkHelper;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -24,6 +32,8 @@ public class DirectoryServiceImp implements DirectoryService {
     private final UserService userService;
     private final StorageService storageService;
     private final ModelMapper modelMapper;
+    private final HateoasLinkHelper hateoasLinkHelper;
+    private final FileService fileService;
 
     @Override
     public List<FileMetadata> getFilesMetaByDirectoryId(UUID directoryId){
@@ -46,17 +56,81 @@ public class DirectoryServiceImp implements DirectoryService {
     }
 
     @Override
+    public Optional<FileMetadata> getDirectoryById(UUID id) {
+        return fileMetadataRepository.findFileMetadataById(id);
+    }
+
+    @Override
     @Transactional
     public FileMetadata create(FileMetadata fileMetadata) {
         var file = fileMetadataRepository.save(fileMetadata);
         fileMetadataRepository.flush();
-        var a = fileMetadataService.getFullPathById(file.getId());
         storageService.createDirectory(fileMetadataService.getFullPathById(file.getId()));
         return file;
     }
 
     @Override
-    public FileMetadata convertToEntity(FolderDto folderDto) {
-        return modelMapper.map(folderDto, FileMetadata.class);
+    public FileMetadata convertToEntity(FolderDtoRequest folderDto) {
+        var fileMetadata = modelMapper.map(folderDto, FileMetadata.class);
+        var userId = userService.getUserIdBySecurityContext(SecurityContextHolder.getContext());
+        var userOpt = userService.getUserById(userId);
+        fileMetadata.setOwner(userOpt.orElseThrow(
+                () -> new UserDoesNotExistException("User "+ userId + " not found")));
+        var parent = fileMetadataRepository
+                .findFileMetadataById(folderDto.getParentId())
+                .orElse(null);
+
+        fileMetadata.setParent(parent);
+        return fileMetadata;
+    }
+
+    @Override
+    public FileDtoResponse convertToFileDtoResponse(FileMetadata fileMetadata) throws IOException {
+        FileDtoResponse fileDtoResponse = modelMapper.map(fileMetadata, FileDtoResponse.class);
+        hateoasLinkHelper.setLinksToContentFolder(fileDtoResponse);
+        return fileDtoResponse;
+    }
+
+    @Override
+    public FolderDtoResponse convertToFolderDtoResponse(FileMetadata fileMetadata) throws IOException {
+        FolderDtoResponse folderDtoResponse = modelMapper.map(fileMetadata, FolderDtoResponse.class);
+        var list = new ArrayList<FileDtoResponse>();
+        for (FileMetadata metadata : this.getFilesMetaByDirectoryId(fileMetadata.getId())) {
+            FileDtoResponse fileDtoResponse = convertToFileDtoResponse(metadata);
+            list.add(fileDtoResponse);
+        }
+
+        folderDtoResponse.setContent(list);
+        hateoasLinkHelper.setLinksToFolder(folderDtoResponse);
+
+        return folderDtoResponse;
+    }
+
+    @Override
+    public CollectionModel<FileDtoResponse> convertToFileCollectionModel(List<FileDtoResponse> fileDtoResponses) throws IOException {
+        CollectionModel<FileDtoResponse> collectionModel = CollectionModel.of(fileDtoResponses);
+        hateoasLinkHelper.setLinksToRoot(collectionModel);
+
+        return collectionModel;
+    }
+
+    @Override
+    @Transactional
+    public void deleteFolder(UUID id) {
+        var folderOptional = fileMetadataRepository.findFileMetadataById(id);
+        var folder = folderOptional.orElseThrow(() -> new DirectoryNotFoundException(
+                "Directory " + id + " not found"
+        ));
+
+        var path = fileMetadataService.getFullPathById(id);
+        try {
+            fileMetadataRepository.delete(folder);
+        }
+        catch (Exception ex) {
+            throw new DirectoryNotDeletedException(
+                    "Folder " + folder.getName() + " not deleted");
+        }
+
+        storageService.deleteFolder(path);
     }
 }

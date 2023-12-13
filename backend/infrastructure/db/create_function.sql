91a2639a4c7423a7ecd334f4ff99a4b7fd270d36
bakan <bakanov-2210@mail.ru>
2023-12-14 00:47:18 +0300

    added create function script

--- backend/infrastructure/db/create_function.sql
+++ backend/infrastructure/db/create_function.sql
@@ -1,19 +1,19 @@
 CREATE OR REPLACE FUNCTION get_full_path(dir_id UUID)
     RETURNS TEXT AS $$
 DECLARE
-    result TEXT;
+result TEXT;
 BEGIN
-    WITH RECURSIVE path_cte(id, parent_id, owner_id, full_path) AS (
-        SELECT id, parent_id, owner_id, CAST(owner_id AS TEXT) || '/' || CAST(id AS TEXT)
-        FROM file_metadata
-        WHERE parent_id IS NULL
-        UNION ALL
-        SELECT d.id, d.parent_id, d.owner_id,
-               pc.full_path || '/' || d.id::TEXT
-        FROM file_metadata d
-                 INNER JOIN path_cte pc ON pc.id = d.parent_id
-    )
-    SELECT full_path INTO result FROM path_cte WHERE id = dir_id;
-    RETURN result;
+WITH RECURSIVE path_cte(id, parent_id, owner_id, full_path) AS (
+    SELECT id, parent_id, owner_id, CAST(owner_id AS TEXT) || '/' || CAST(id AS TEXT)
+    FROM file_metadata
+    WHERE parent_id IS NULL
+    UNION ALL
+    SELECT d.id, d.parent_id, d.owner_id,
+           pc.full_path || '/' || d.id::TEXT
+    FROM file_metadata d
+             INNER JOIN path_cte pc ON pc.id = d.parent_id
+)
+SELECT full_path INTO result FROM path_cte WHERE id = dir_id;
+RETURN result;
 END;
 $$ LANGUAGE plpgsql;
